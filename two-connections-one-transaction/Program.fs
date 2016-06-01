859d13e5be7cc911f90de949985175555f012789
Kristof Mattei <github@kristofmattei.be>
2016-06-01 10:54:36 +0200

    Initial commit

---
+++ two-connections-one-transaction/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    
+    use tran = new TransactionScope () 
+
+
+    let conn1 = new SqlConnection () 
+    conn1.Open ()
+
+    // do work
+
+    conn1.Close ()
+
+
+    let conn2 = new SqlConnection ()
+
+    conn2.Open ()
+
+    conn2.Close ()
+
+
+    // work rolled back?
+    // don't complete
+    0
